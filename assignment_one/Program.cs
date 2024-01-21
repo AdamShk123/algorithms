600dc0b94b44c58a1cd937c6663a4a5f19ab5ebb
AdamShk123 <adamshkolnik5@gmail.com>
2024-01-21 16:02:53 -0600

    added new project for first assignment

---
+++ assignment_one/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace assignment_one
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
